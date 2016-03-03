00cd5cd7bd0051d5fbf26434cb6e15b8ca1d46d6
arkgnan <dedi.ananto@gmail.com>
2016-03-03 10:38:49 +0700

    Update header.php

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -95,9 +95,9 @@
 		<!-- Logo -->
 		<a href="<?php echo site_url(); ?>" class="logo">
 		  <!-- mini logo for sidebar mini 50x50 pixels -->
-		  <span class="logo-mini"><img src="<?php echo base_url('assets/img/lp1.png');?>"/></span>
+		  <span class="logo-mini"><b>A</b>LT</span>
 		  <!-- logo for regular state and mobile devices -->
-		  <span class="logo-lg"><img src="<?php echo base_url('assets/img/lp2.png');?>"/></span>
+		  <span class="logo-lg"><b>Admin</b>LTE</span>
 		</a>
 		
 		<!-- Header Navbar -->

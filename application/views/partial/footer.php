062c606d5cb7f0f01e316f49fc2e7f06b7f1fe69
arkgnan <dedi.ananto@gmail.com>
2016-03-03 10:36:49 +0700

    Update footer.php

--- application/views/partial/footer.php
+++ application/views/partial/footer.php
@@ -15,19 +15,8 @@
 			<b>Version</b> 0.1 (alpha)
 		</div>
 		<!-- Default to the left -->
-		<strong>Copyright &copy; 2015 <a href="http://loginpos.com" target="_blank">Loginpos</a>.</strong> All rights reserved.
+		<strong>Copyright &copy; 2015 <a href="http://www.opensourceposguide.com/guide/gettingstarted/installation" target="_blank">opensourcepos</a>.</strong> All rights reserved.
 	</footer>
-	<!--
-	<div id="footer">
-		<div class="jumbotron">
-			<?php echo $this->lang->line('common_you_are_using_ospos'); ?>
-			<?php echo $this->config->item('application_version'); ?>.
-			<?php echo $this->lang->line('common_please_visit_my'); ?>
-			<a href="https://github.com/jekkos/opensourcepos" target="_blank"><?php echo $this->lang->line('common_website'); ?></a>
-			<?php echo $this->lang->line('common_learn_about_project'); ?>
-		</div>
-	</div>
-	-->	
 	</div>
 	<!-- ./wrapper -->
 </body>
